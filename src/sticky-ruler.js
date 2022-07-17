c02c7c30b383e120f1a9988799befbafba155334
Forrest Cuevas <fscottc8@gmail.com>
2022-07-17 12:23:50 -0400

    Cleaned up first pass at sticky ruler, added dynamic use of terrain-ruler

--- src/sticky-ruler.js
+++ src/sticky-ruler.js
@@ -16,51 +16,43 @@ Hooks.on('ready', function() {
 
 function rulerLeftClick(wrapped, event, ...args) {
 	// TODO: Check if this player/account type has this option "enabled"
+
+	// Find the center point
+	const center = canvas.grid.getCenter(event.data.origin.x, event.data.origin.y);
+	const centerPoint = new PIXI.Point(center[0], center[1]);
+
 	const token = event.target
 	if (token instanceof Token && token.owner && this.waypoints.length == 0) {
 		token.control(false);
 		this._onDragStart(event);
-		this.measure(event.data.origin);
+		this.measure(centerPoint);
 
-		// TODO: Use drag ruler dynamically (or implement my own path finding?)
-		// TODO: There is an issue with moving around things and using drag-ruler
-		// this.dragRulerAddWaypoint(event.data.origin);
-		// this.measure(event.data.origin);
-		// this.draggedEntity = token;
+		if (game.modules.get("terrain-ruler")?.active) {
+			// If terrain-ruler is installed, enable it and the terrain
+			canvas.terrain.visible = true;
+			this.isTerrainRuler = true;
+		}
 
-		// TODO: Use terrain ruler dynamically (or implement my own version?)
-		// canvas.terrain.visible = true;
-		// this.isTerrainRuler = true;
 		return wrapped(event, ...args);
 	}
 
-	const center = canvas.grid.getCenter(event.data.origin.x, event.data.origin.y);
-	const point = new PIXI.Point(center[0], center[1]);
-	const location = this.waypoints.findIndex((waypoint) => waypoint.equals(point));
+	// Find the center point and see if it's already a waypoint
+	const location = this.waypoints.findIndex((waypoint) => waypoint.equals(centerPoint));
 
 	if (location == -1)
 	{
-		// New potential location found; Check for collision first
-		let rays = this._getRaysFromWaypoints(this.waypoints.slice(-1), event.data.origin);
-		let hasCollision = rays.some(r => canvas.walls.checkCollision(r));
-		if (!hasCollision) {
+		if (!game.user.isGM && isHostileToken(token)) {
+			ui.notifications.error("ERROR: You cannot move through a hostile!", {localize: true});
+			return wrapped(event, ...args);
+		}
+
+		// New potential location found; Check for collision
+		if (!rulerCollision(this, centerPoint)) {
 			// Collision wasn't found, add a new waypoint
 			this._state = Ruler.STATES.MEASURING;
-			this._addWaypoint(event.data.origin);
-			this.measure(event.data.origin);
-
-			// TODO: Use drag ruler dynamically?
-			// this.measure(event.data.origin);
-			// this.dragRulerAddWaypoint(event.data.origin);
-
-			// Broadcast updated waypoint
-			const sc = game.user.hasPermission("SHOW_CURSOR");
-			const sr = game.user.hasPermission("SHOW_RULER");
-			if (sc || sr) {
-				const position = event.data.getLocalPosition(this);
-				const ruler = sr ? this.toJSON() : null;
-				game.user.broadcastActivity({cursor: position, ruler: ruler});
-			}
+			this._addWaypoint(centerPoint);
+			this.measure(centerPoint);
+			broadcastRulerUpdate(this, event);
 		} else {
 			// Collision found, print error message and do not add waypoint
 			ui.notifications.error("ERROR.TokenCollide", {localize: true});
@@ -72,10 +64,8 @@ function rulerLeftClick(wrapped, event, ...args) {
 	} else {
 		// This is a previous waypoint, remove all after it
 		const diff = this.waypoints.length - location - 1;
-		console.log(diff)
-		console.log("")
 		for (var i = 0; i < diff; i++) {
-			this._removeWaypoint(event.data.origin);
+			this._removeWaypoint(centerPoint);
 		}
 	}
 
@@ -83,5 +73,25 @@ function rulerLeftClick(wrapped, event, ...args) {
 }
 
 function isHostileToken(token) {
+	if (!(token instanceof Token)) {
+		return false;
+	}
 	return token.data.disposition == CONST.TOKEN_DISPOSITIONS.HOSTILE;
+}
+
+// TODO: Check for hostile collision tokens too?
+function rulerCollision(ruler, point) {
+	let rays = ruler._getRaysFromWaypoints(ruler.waypoints.slice(-1), point);
+	return rays.some(r => canvas.walls.checkCollision(r));
+}
+
+function broadcastRulerUpdate(ruler, event) {
+	// Broadcast updated ruler information
+	const sc = game.user.hasPermission("SHOW_CURSOR");
+	const sr = game.user.hasPermission("SHOW_RULER");
+	if (sc || sr) {
+		const position = event.data.getLocalPosition(ruler);
+		const sRuler = sr ? ruler.toJSON() : null;
+		game.user.broadcastActivity({cursor: position, ruler: sRuler});
+	}
 }
