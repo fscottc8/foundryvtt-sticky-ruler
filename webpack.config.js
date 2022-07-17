2b64108da373218f5831203462b4cce9382d12a3
Forrest Cuevas <fscottc8@gmail.com>
2022-07-17 15:08:26 -0400

    Adding changes to module and webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/sticky-ruler.js',
   output: {
     filename: 'sticky-ruler.js',
-    // path: path.resolve(__dirname, 'dist'),
-    path: path.resolve('C:/Users/Scott/AppData/Local/FoundryVTT/Data/modules/sticky-ruler', 'dist'),
+    path: path.resolve(__dirname, 'dist'),
+    // path: path.resolve('C:/Users/Scott/AppData/Local/FoundryVTT/Data/modules/sticky-ruler', 'dist'),
   }
 }
