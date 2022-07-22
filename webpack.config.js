1aa255b1d7d8bf68fc7ce803be59820e65f0eb1d
Forrest Cuevas <fscottc8@gmail.com>
2022-07-21 23:13:48 -0400

    Updating webpack script

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   entry: './src/sticky-ruler.js',
   output: {
     filename: 'sticky-ruler.js',
-    // path: path.resolve(__dirname, 'dist'),
-    path: path.resolve('C:/Users/Scott/AppData/Local/FoundryVTT/Data/modules/sticky-ruler', 'dist'),
+    path: path.resolve(__dirname, 'dist'),
   }
 }
